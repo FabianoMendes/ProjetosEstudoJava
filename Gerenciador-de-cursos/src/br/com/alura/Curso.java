f5468cd8f276f41d48a62a17187f665ee4e7a4d2
FabianoMendes <fabianomendesdefreitas@gmail.com>
2016-06-24 03:32:58 -0300

    Commit

--- Gerenciador-de-cursos/src/br/com/alura/Curso.java
+++ Gerenciador-de-cursos/src/br/com/alura/Curso.java
@@ -1,13 +1,19 @@
 package br.com.alura;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Curso {
 	private String nome;
 	private String instrutor;
 	private List<Aula> aulas = new LinkedList<Aula>();
+	private Set<Aluno> alunos = new HashSet<>();
+	private Map<Integer, Aluno> matriculaParaAluno = new HashMap<>();
 
 	public Curso(String nome, String instrutor) {
 		this.nome = nome;
@@ -28,7 +34,15 @@ public class Curso {
 	
 	public void adiciona(Aula aula) {
 		this.aulas.add(aula);
-
+	}
+	
+	public void matricula(Aluno aluno){
+	    this.alunos.add(aluno);
+	    this.matriculaParaAluno.put(aluno.getMatricula(), aluno);
+	}
+	
+	public Set<Aluno> getAlunos() {
+	    return Collections.unmodifiableSet(alunos);
 	}
 	
 	public  int getTempoTotal() {
@@ -43,4 +57,15 @@ public class Curso {
 	public String toString() {
 		return "[Curso:" + nome + " tempo total:" + getTempoTotal() + " aulas:" + this.aulas + "]";
 	}
+	
+	private boolean estaMatriculado(Aluno aluno) {
+		return this.alunos.contains(aluno);
+
+	}
+	
+	private Aluno buscaMatriculado(int numero) {
+		return matriculaParaAluno.get(numero);
+	}
+	
+	
 }
